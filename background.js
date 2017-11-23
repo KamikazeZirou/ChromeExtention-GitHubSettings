2eff5dfd8df7699f5240caac43d01bd65e9afb29
KamikazeZirou <kamikaze.zirou@gmail.com>
2017-11-23 18:55:41 +0900

    Change the action type from browser to page.

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'github.com' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostContains: "github.com" },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
 var settings = {};
 
 chrome.storage.sync.get({
@@ -38,7 +57,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     urls: [
       "https://github.com/*"
     ],
-    types: ["main_frame"]
+    types: ["main_frame", "xmlhttprequest"]
   },
   // extraInfoSpec
   ["blocking"]
